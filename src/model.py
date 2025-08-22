82023ec8ca481ff58334f666272226ce6bfe4608
Margaret Tam <koapa13@gmail.com>
2025-08-22 12:18:07 -0700

    set local_files_only to force using cache files only, no downloads

--- src/model.py
+++ src/model.py
@@ -7,7 +7,7 @@ from diffusers import DDPMScheduler
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def make_1step_sched():
-    noise_scheduler_1step = DDPMScheduler.from_pretrained("stabilityai/sd-turbo", subfolder="scheduler")
+    noise_scheduler_1step = DDPMScheduler.from_pretrained("stabilityai/sd-turbo", subfolder="scheduler", local_files_only=True)
     noise_scheduler_1step.set_timesteps(1, device=device)
     if torch.cuda.is_available():
         noise_scheduler_1step.alphas_cumprod = noise_scheduler_1step.alphas_cumprod.cuda()
--- src/pix2pix_turbo.py
+++ src/pix2pix_turbo.py
@@ -29,13 +29,13 @@ class TwinConv(torch.nn.Module):
 class Pix2Pix_Turbo(torch.nn.Module):
     def __init__(self, pretrained_name=None, pretrained_path=None, ckpt_folder="checkpoints", lora_rank_unet=8, lora_rank_vae=4):
         super().__init__()
-        self.tokenizer = AutoTokenizer.from_pretrained("stabilityai/sd-turbo", subfolder="tokenizer")
-        self.text_encoder = CLIPTextModel.from_pretrained("stabilityai/sd-turbo", subfolder="text_encoder")
+        self.tokenizer = AutoTokenizer.from_pretrained("stabilityai/sd-turbo", subfolder="tokenizer", local_files_only=True)
+        self.text_encoder = CLIPTextModel.from_pretrained("stabilityai/sd-turbo", subfolder="text_encoder", local_files_only=True)
         if torch.cuda.is_available():
             self.text_encoder = self.text_encoder.cuda()
         self.sched = make_1step_sched()
 
-        vae = AutoencoderKL.from_pretrained("stabilityai/sd-turbo", subfolder="vae")
+        vae = AutoencoderKL.from_pretrained("stabilityai/sd-turbo", subfolder="vae", local_files_only=True)
         vae.encoder.forward = my_vae_encoder_fwd.__get__(vae.encoder, vae.encoder.__class__)
         vae.decoder.forward = my_vae_decoder_fwd.__get__(vae.decoder, vae.decoder.__class__)
         # add the skip connection convs
@@ -49,7 +49,7 @@ class Pix2Pix_Turbo(torch.nn.Module):
             vae.decoder.skip_conv_3 = vae.decoder.skip_conv_3.cuda()
             vae.decoder.skip_conv_4 = vae.decoder.skip_conv_4.cuda()
         vae.decoder.ignore_skip = False
-        unet = UNet2DConditionModel.from_pretrained("stabilityai/sd-turbo", subfolder="unet")
+        unet = UNet2DConditionModel.from_pretrained("stabilityai/sd-turbo", subfolder="unet", local_files_only=True)
 
         if pretrained_name == "edge_to_image":
             url = "https://www.cs.cmu.edu/~img2img-turbo/models/edge_to_image_loras.pkl"
